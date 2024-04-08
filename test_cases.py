d96536a823465f0ec5875fc23a40e794ddf8b0c2
Saurabh Bairagi <105032711+BairagiSaurabh@users.noreply.github.com>
2024-04-08 16:32:47 +0530

    bugs fixed

--- test_cases.py
+++ test_cases.py
@@ -1,31 +1,7 @@
 from main import Data_Preprocess
-
 import pandas as pd
-import feature_engine
-import numpy as np
-#import streamlit as st
-import pandas as pd
-import dtale
-from sklearn.model_selection import train_test_split
-from feature_engine.imputation import (
-    MeanMedianImputer,
-    CategoricalImputer,
-    ArbitraryNumberImputer
-)
-from feature_engine.encoding import WoEEncoder
-from feature_engine.selection import DropConstantFeatures
-from feature_engine.creation import CyclicalFeatures
-from feature_engine.encoding import OneHotEncoder
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.preprocessing import LabelEncoder
-from feature_engine.encoding import CountFrequencyEncoder
-from feature_engine.selection import DropCorrelatedFeatures
-from feature_engine.outliers import Winsorizer, ArbitraryOutlierCapper, OutlierTrimmer
-import warnings
-warnings.filterwarnings('ignore')
 
 df = pd.read_csv("project_donor.csv",na_values='Nan')
 test = Data_Preprocess(df,"project_donor_processed")
 test.run()
 
-# company_id,refund_date,party_regdate
