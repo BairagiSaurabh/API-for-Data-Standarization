93f4e192ceb8058c94dac96195ea956fdcc1faa2
Saurabh Bairagi <105032711+BairagiSaurabh@users.noreply.github.com>
2024-04-09 15:38:18 +0530

    code updated

--- test_cases.py
+++ test_cases.py
@@ -1,7 +1,6 @@
 from main import Data_Preprocess
 import pandas as pd
 
-df = pd.read_csv("project_donor.csv",na_values='Nan')
-test = Data_Preprocess(df,"project_donor_processed")
-test.run()
-
+df = pd.read_csv("Coffee_Qlty_By_Country.csv",na_values='Nan')
+test = Data_Preprocess(df,"Coffee_Qlty_By_Country_trial")
+test.run()  
